517f052b1f7d29b20c61a4ee3c8810f33c12d966
BlitzKraig <craig@blitzkraig.com>
2019-07-12 17:52:18 +0100

    Moved livecapture from tests to root to allow for proper exe generation

---
+++ sp108e-control-module/livecapture.js
@@ -0,0 +1,4 @@
+const sp108e = require('./sp108e');
+const messages = require('./messages');
+
+sp108e.captureDesktop();
--- sp108e-control-module/test/livecapture.js
+++
@@ -1,5 +0,0 @@
-const sp108e = require('../sp108e');
-const messages = require('../messages');
-
-sp108e.captureDesktop();
-// sp108e.close();
