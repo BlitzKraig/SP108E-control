26593149cefa0eb7d4029448e234369bac4961c8
BlitzKraig <craig@blitzkraig.com>
2019-07-13 01:08:39 +0100

    Updated gif test script

--- sp108e-control-module/test/gif.js
+++ sp108e-control-module/test/gif.js
@@ -1,4 +1,21 @@
 const sp108e = require('../sp108e');
 const messages = require('../messages');
 
-sp108e.playGif('./input/redbar.gif', 50, false);
+var gif = 'redbar.gif'
+var time = 50;
+var loop = true;
+
+if (process.argv[2]) {
+    console.log(process.argv[2]);
+    gif = process.argv[2];
+}
+if (process.argv[3]) {
+    console.log(process.argv[3]);
+    time = process.argv[3];
+}
+if (process.argv[4]) {
+    console.log(process.argv[4]);
+    loop = process.argv[4];
+}
+
+sp108e.playGif(`./input/${gif}`, time, loop);
